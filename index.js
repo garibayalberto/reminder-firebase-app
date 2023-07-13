bb95f2acf26d9b2122de38a8c0734a63b7ff521f
Alberto <garibayalberto98@gmail.com>
2023-07-12 21:10:10 -0700

    Finished Firebase setup and added a few modifications for testing

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const {appSettings} = require('./secrets.js');
+import { appSettings } from "./secrets.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push  } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
 
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
 
 addButtonEl.addEventListener("click" , function() {
   let inputValue = inputFieldEl.value;
-  console.log(inputValue);
+  push(shoppingListInDB, inputValue);
+  //console.log(inputValue);
 });
+
